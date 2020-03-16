30af41d332516856b42ba2388bd371324530b9e2
Martin Sone <kwangmart12@gmail.com>
2020-03-16 21:55:11 +0100

    Teams names added at the UI

--- src/Components/ui/matches_block.js
+++ src/Components/ui/matches_block.js
@@ -11,9 +11,9 @@ const MatchesBlock = ({match}) => {
                  <div className ="match_top">
                      <div className="left">
                          <div className="icon" style={{background: `url(/images/team_icons/${match.localThmb}.png)`}}></div>
+    <div className="team_name">{match.local}</div>
 
                         
-
                      </div>
                       <div className="right">
 
