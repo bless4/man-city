0bfa242a902ea6e4254c3f8e42367bdc13ef0668
Martin Sone <kwangmart12@gmail.com>
2020-04-01 09:24:30 +0200

    block.js music.js meetplayers.js and playérscard.js modified

--- src/Components/home/matches/Blocks.js
+++ src/Components/home/matches/Blocks.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {firebaseMatches} from '../../../firebase';
 import {firebaseLooper, reverseArray} from '../../ui/misc';
 import MatchesBlock from '../../ui/matches_block';
+import Slide from 'react-reveal/Slide';
 
 class Blocks extends Component {
 
@@ -22,12 +23,14 @@ class Blocks extends Component {
     showMatches=(matches)=> (
         matches?
         matches.map((match)=>(
+            <Slide bottom key={match.id}>
             <div className="item">
                 <div className="wrapper">
                        <MatchesBlock match={match}/>
                 </div>
 
             </div>
+            </Slide>
         ))
         :null
     )
