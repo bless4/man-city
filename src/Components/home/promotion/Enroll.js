3acc26fa1eedfa0478b48b192aeb1c45a288f44d
Martin Sone <kwangmart12@gmail.com>
2020-05-06 11:29:20 +0200

    enroll.js added

---
+++ src/Components/home/promotion/Enroll.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
+
+class Enroll extends Component {
+
+    state = {
+        formError:false
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Enroll;
