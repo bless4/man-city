dff443190557a769850ad6ac89ec6e24db47d295
Martin Sone <kwangmart12@gmail.com>
2020-05-06 11:52:28 +0200

    added enrollment form

--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,27 @@
-import  firebase from 'firebase/app';
-//import 'firebase/app'
+import firebase from 'firebase/app';
+import 'firebase/app'
 import 'firebase/database'
 
 
 const Config = {
-    apiKey: "AIzaSyB1JJFiwVKe_M5VRRdUuGDUnkqdsSuTYXw",
-    authDomain: "m-city-c2c20.firebaseapp.com",
-    databaseURL: "https://m-city-c2c20.firebaseio.com",
-    projectId: "m-city-c2c20",
-    storageBucket: "m-city-c2c20.appspot.com",
-    messagingSenderId: "1036248094123",
-    appId: "1:1036248094123:web:910b76a60f5a867b"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(Config);
+  apiKey: "AIzaSyB1JJFiwVKe_M5VRRdUuGDUnkqdsSuTYXw",
+  authDomain: "m-city-c2c20.firebaseapp.com",
+  databaseURL: "https://m-city-c2c20.firebaseio.com",
+  projectId: "m-city-c2c20",
+  storageBucket: "m-city-c2c20.appspot.com",
+  messagingSenderId: "1036248094123",
+  appId: "1:1036248094123:web:910b76a60f5a867b"
+};
+// Initialize Firebase
+firebase.initializeApp(Config);
 
- const firebaseDB = firebase.database();
- 
- const firebaseMatches= firebaseDB.ref('matches');
- 
-  export {
-   firebase,
-   firebaseMatches
-  };
+const firebaseDB = firebase.database();
+
+const firebaseMatches = firebaseDB.ref('matches');
+const firebasePromotions = firebaseDB.ref('promotions');
+
+export {
+  firebase,
+  firebaseMatches,
+  firebasePromotions
+}
