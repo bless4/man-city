0fa55f098697bd95a3c318e64ddb9ed977db2646
Martin Sone <kwangmart12@gmail.com>
2020-05-07 09:38:11 +0200

    login component created

--- src/Components/signin/index.js
+++ src/Components/signin/index.js
@@ -1,48 +1,49 @@
 import React, { Component } from 'react';
+import { firebase } from '../../firebase';
 import FormField from '../ui/formFields';
 import { validate } from '../ui/misc';
 class SignIn extends Component {
-    
+
     state = {
-        formError:false,
-        formSuccess:'',
-        formdata:{
-            email:{
-                element:'input',
-                value:'',
-                config:{
-                    name:'email_input',
+        formError: false,
+        formSuccess: '',
+        formdata: {
+            email: {
+                element: 'input',
+                value: '',
+                config: {
+                    name: 'email_input',
                     type: 'email',
                     placeholder: 'Enter your email'
                 },
-                validation:{
+                validation: {
                     required: true,
                     email: true
                 },
                 valid: false,
-                validationMessage:''
+                validationMessage: ''
             },
-            password:{
-                element:'input',
-                value:'',
-                config:{
-                    name:'password_input',
+            password: {
+                element: 'input',
+                value: '',
+                config: {
+                    name: 'password_input',
                     type: 'password',
-                    placeholder: 'Enter your email'
+                    placeholder: 'Enter your password'
                 },
-                validation:{
+                validation: {
                     required: true
-                    
+
                 },
                 valid: false,
-                validationMessage:''
+                validationMessage: ''
             }
         }
 
     }
-    updateForm(element){
-        const newFormdata = {...this.state.formdata}
-        const newElement = { ...newFormdata[element.id]}
+    updateForm(element) {
+        const newFormdata = { ...this.state.formdata }
+        const newElement = { ...newFormdata[element.id] }
 
         newElement.value = element.event.target.value;
 
@@ -59,19 +60,32 @@ class SignIn extends Component {
     }
 
 
-    submitForm(event){
+    submitForm(event) {
         event.preventDefault();
-        
+
         let dataToSubmit = {};
         let formIsValid = true;
 
-        for(let key in this.state.formdata){
+        for (let key in this.state.formdata) {
             dataToSubmit[key] = this.state.formdata[key].value;
             formIsValid = this.state.formdata[key].valid && formIsValid;
         }
 
-        if(formIsValid){
-          console.log(dataToSubmit)
+        if (formIsValid) {
+
+            firebase.auth()
+                .signInWithEmailAndPassword(
+                    dataToSubmit.email,
+                    dataToSubmit.password
+                ).then(() => {
+
+                    this.props.history.push('/dashboard')
+                }).catch(error => {
+                    this.setState({
+                        formError: true
+                    })
+                })
+
             //this.resetFormSuccess()
         } else {
             this.setState({
@@ -83,26 +97,30 @@ class SignIn extends Component {
     render() {
         return (
             <div className="container">
-                <div className ="signin_wrapper" style ={{margin: '100px'}}>
-                     <form onSubmit ={(event)=> this.submitForm(event)}>
-                         <h2>Please Login</h2>
-                         <FormField
-                                id={'email'}
-                                formdata={this.state.formdata.email}
-                                change={(element)=> this.updateForm(element)}
-                            />
+                <div className="signin_wrapper" style={{ margin: '100px' }}>
+                    <form onSubmit={(event) => this.submitForm(event)}>
+                        <h2>Please Login</h2>
+                        <FormField
+                            id={'email'}
+                            formdata={this.state.formdata.email}
+                            change={(element) => this.updateForm(element)}
+                        />
 
-                             <FormField
-                                id={'password'}
-                                formdata={this.state.formdata.password}
-                                change={(element)=> this.updateForm(element)}
-                            />
-                             <button onClick={(event)=> this.submitForm(event)}>Login</button>
+                        <FormField
+                            id={'password'}
+                            formdata={this.state.formdata.password}
+                            change={(element) => this.updateForm(element)}
+                        />
+                        {this.state.formError ?
+                            <div className="error_label">Email or Password is incorrect! Try again.</div>
+                            : null
+                        }
+                        <button onClick={(event) => this.submitForm(event)}>Login</button>
 
-                     </form>
+                    </form>
 
                 </div>
-                
+
             </div>
         );
     }
