f2e0518e28ab4e799a906396c935ff1d0d5599c6
Martin Sone <kwangmart12@gmail.com>
2020-07-07 22:23:47 +0200

    admin_matches route added

---
+++ src/Components/admin/matches/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {link} from 'react-router-dom';
+import AdminLayout from '../../../Hoc/AdminLayout';
+
+
+
+
+class AdminMatches extends Component {
+    render() {
+        return (
+            <AdminLayout>
+               <h1>table</h1> 
+            </AdminLayout>
+        );
+    }
+}
+
+export default AdminMatches;
--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import Layout from './Hoc/Layout';
-import { Switch, Route }  from 'react-router-dom';
-import PrivateRoute from './Components/authRoutes/PrivateRoutes';
+import { Switch, Route } from 'react-router-dom';
+import PrivateRoutes from './Components/authRoutes/PrivateRoutes';
+import PublicRoutes from './Components/authRoutes/PublicRoutes';
 import SignIn from './Components/signin/index';
 import Dashboard from './Components/admin/Dashboard';
+import AdminMatches from './Components/admin/matches';
 
 import Home from './Components/home';
 
 const Routes = (props) => {
-  return(
+  return (
     <Layout>
-        <Switch>
-            <Route exact component={Home} path="/"/>
-            <Route exact component={SignIn} path ="/SignIn"/>
-            
-            <PrivateRoute {...props} path="/dashboard" exact component={Dashboard}/>
-        </Switch>
+      <Switch>
+        <PublicRoutes {...props} restricted={true} exact component={SignIn} path='/signin' />
+        <PublicRoutes {...props} restricted={false} exact component={Home} path='/' />
+         
+        <PrivateRoutes exact component={Dashboard} path='/dashboard' {...props} />
+          <PrivateRoutes exact component={AdminMatches} path='/admin_matches' {...props} />
+        
+      </Switch>
     </Layout>
   )
 }
